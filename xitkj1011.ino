faf8ce713e37db5ddd277476e7ef580296c33056
elokmaryati <elokmaryati1101@gmail.com>
2016-09-18 16:10:09 +0700

    Add files via upload

---
+++ xitkj1011.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(12,OUTPUT);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(12, HIGH);//Pada pin 12, LED menyala
+  delay(1500);
+  digitalWrite(12, LOW);//Pada pin 12, LED mati
+  delay(1500); //Tunda 1,5 sec pada saat mati
+}
